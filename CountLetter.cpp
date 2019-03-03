ac0cd3d89c54029a60f965f3813489077a72b01f
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2019-03-03 15:50:04 -0800

    Compiles

--- CountLetter.cpp
+++ CountLetter.cpp
@@ -1,30 +1,10 @@
 #include <iostream>
-#include<bits/stdc++.h>
-
 using namespace std;
 
-int main()
+string CountLetter(string sentence, char searching, int counter)
 {
-        char searching = '\0';
-        string sentence ="";
-        int counter = 0;
-        char l2;
 
-        cout<<"Enter a sentence."<<endl;
-        getline (cin, sentence);
-        cout<<"Enter a letter."<<endl;
-        cin>>searching;
-        transform(sentence.begin(), sentence.end(), sentence.begin(), ::tolower); 
 
-        for (int i = 0; i < sentence.length(); i++)
-        {
-                
-                if (sentence [i] == searching)
-                {
-                        counter++;
-                }
-        }
-        cout<<"The letter "<<searching<<" occurred "<<counter<<" time(s)."<<endl;
 
-        return 0;
+
 }
