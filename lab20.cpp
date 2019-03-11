1ecc96439c97aeaef115bbcaba8ed571a5236456
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2019-03-10 21:05:51 -0700

    lab20.cpp

---
+++ lab20.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+        string name;
+        string fname;
+        int bal;
+
+
+        cout<<"Enter your first name."<<endl;
+        getline (cin, name);
+        ifstream inx (name);
+        ofstream outx (name);
+
+        if (inx.good())
+        {
+                cout<<"Welcome back "<< fname << endl;
+                cout<<"Here is a log of all your BALANCE activity."<<endl;
+                cout<<"Your balance is 1000"<<endl;
+
+        }
+        
+        else
+        {
+
+                cout<<"Enter your FIRST and LAST name."<<endl;
+                getline (cin, fname);
+                outx<<fname<<endl;
+
+                cout<<"Hello "<<fname<<endl;
+                cout<<"Enter your initial balance."<<endl;
+                cin>>bal;
+                
+                
+                outx<<bal<<endl;
+                
+
+                outx.close();
+
+        }
+        return 0;
+}
